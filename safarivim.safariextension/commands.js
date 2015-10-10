ff504099a2d864b783b7795c263c34192bc0276f
VirrageS <virrages@gmail.com>
2015-10-10 16:15:00 +0200

    Removed bug with empty text and clearing links

--- safarivim.safariextension/commands.js
+++ safarivim.safariextension/commands.js
@@ -11,7 +11,6 @@ function smoothScroll(x, y) {
     }
 }
 
-vimsafari.opened = false;
 vimsafari.openedLinks = [];
 
 var commands = [
@@ -39,7 +38,6 @@ var commands = [
 
     ["f", function(count) {
         var openedLinks = vimsafari.openedLinks;
-        var opened = vimsafari.opened;
 
         function removeOpenedLinks() {
             Element.prototype.remove = function() {
@@ -60,58 +58,55 @@ var commands = [
             vimsafari.openedLinks = [];
         }
 
-        if (opened === true) {
-            removeOpenedLinks();
-        } else {
-            var links = document.getElementsByTagName("a");
-            var currentCommands = commands.length;
+        var links = document.getElementsByTagName("a");
+        var currentCommands = commands.length;
 
-            var combinations = {};
-            for (var i = 0; i < links.length; i++) {
-                if (links[i].href) {
-                    var tooltip = document.createElement("div");
-                    tooltip.id = i;
-                    tooltip.style.cssText = '\
-                      text-indent: 0; border: none;display: block;font: normal;letter-spacing: normal;line-height: normal;margin: 0;padding: 0;text-transform: normal;visibility: visible;width: auto;word-spacing: normal; z-index: auto;\
-                      clear: none; float: none;\
-                      background: yellow;\
-                      width: 20px; height: 20px;\
-                      position: absolute; top: 2px; left: 2px; bottom: auto;\
-                      border: 1px solid black; border-radius: 2px;\
-                      color: black; font-size: 9px !important; text-align: center; vertical-align: middle !important; font-weight: 300;';
+        var combinations = {};
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].href) {
+                var tooltip = document.createElement("div");
+                tooltip.id = i;
+                tooltip.style.cssText = '\
+                  text-indent: 0; border: none;display: block;font: normal;letter-spacing: normal;line-height: normal;margin: 0;padding: 0;text-transform: normal;visibility: visible;width: auto;word-spacing: normal; z-index: auto;\
+                  clear: none; float: none;\
+                  background: yellow;\
+                  width: 20px; height: 20px;\
+                  position: absolute; top: 2px; left: 2px; bottom: auto;\
+                  border: 1px solid black; border-radius: 2px;\
+                  color: black; font-size: 9px !important; text-align: center; vertical-align: middle !important; font-weight: 300;';
 
-                    var text = "";
-                    var possible = "abcegimnopqrstvwyz"; // BCEGIMNOPQRSTVWYZ
+                var text = "";
+                var possible = "abcegimnopqrstvwyz"; // BCEGIMNOPQRSTVWYZ
 
-                    // check if combination is unique
-                    while (combinations[text]) {
-                        text = "";
-                        for (var j = 0; j < 2; j++) {
-                            text += possible.charAt(Math.floor(Math.random() * possible.length));
-                        }
-
-                        // TODO: check if 26^2 combinations have not been used
+                // check if combination is unique
+                while (combinations[text] || text === "") {
+                    text = "";
+                    for (var j = 0; j < 2; j++) {
+                        text += possible.charAt(Math.floor(Math.random() * possible.length));
                     }
 
-                    combinations[text] = true;
-
-                    tooltip.innerHTML = text;
-                    links[i].appendChild(tooltip);
-                    openedLinks.push(tooltip.id);
-
-                    vimsafari.registerLink(text, (function(url) {
-                        return function(count) {
-                            safari.self.tab.dispatchMessage("openTabWithLink", url);
-
-                            removeOpenedLinks();
-                            // TODO: remove registeredEvents
-                        }
-                    })(links[i].href));
+                    // TODO: check if 26^2 combinations have not been used
                 }
+
+                combinations[text] = true;
+
+                tooltip.innerHTML = text;
+                links[i].appendChild(tooltip);
+                openedLinks.push(tooltip.id);
+
+                vimsafari.registerLink(text, (function(url) {
+                    return function(count) {
+                        safari.self.tab.dispatchMessage("openTabWithLink", url);
+
+                        removeOpenedLinks();
+                    }
+                })(links[i].href));
             }
         }
 
-        vimsafari.opened = !vimsafari.opened;
+        vimsafari.registerLink("f", function() {
+            removeOpenedLinks();
+        });
     }]
 ];
 
